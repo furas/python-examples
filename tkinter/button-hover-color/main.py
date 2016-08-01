9c090490a234e07386f63258e9ca45b95f034e2d
Bartlomiej "furas" Burek <furas@tlen.pl>
2016-08-01 08:51:03 +0200

    button hover color

---
+++ tkinter/button-hover-color/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''button hover color'''
+
+import tkinter as tk
+
+
+root = tk.Tk()
+
+# default hover color
+button0 = tk.Button(root, text='Default hover color')
+button0.pack()
+
+# own hover color
+button1 = tk.Button(root, text='Own hover color', activebackground='red')
+button1.pack()
+
+# none hover color
+button2 = tk.Button(root, text='None hover color')
+button2.config(activebackground=button2.cget('background'))
+button2.pack()
+
+# activebackground='SystemButtonFace' doesn't work on Linux
+
+root.mainloop()
