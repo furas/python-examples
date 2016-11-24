72e014aa4fa52e2128112ea25cfaca32a3862000
Bartlomiej "furas" Burek <furas@tlen.pl>
2016-11-24 02:50:37 +0100

    combobox-get-selection

--- tkinter/combobox-get-selection/main.py
+++ tkinter/combobox-get-selection/main.py
@@ -7,13 +7,12 @@ import tkinter.ttk as ttk
 
 def on_select(event=None):
     print('----------------------------')
-    
+
     if event:
         print("event.widget:", event.widget.get())
 
     for i, x in enumerate(all_comboboxes):
         print("all_comboboxes[%d]: %s" % (i, x.get()))
-
 
 # --- main ---
 
@@ -21,22 +20,21 @@ root = tk.Tk()
 
 all_comboboxes = []
 
-cb = ttk.Combobox(root, values=("1", "2", "3", "4", "5"))     
+cb = ttk.Combobox(root, values=("1", "2", "3", "4", "5"))
 cb.set("1")
 cb.pack()
 cb.bind('<<ComboboxSelected>>', on_select)
 
 all_comboboxes.append(cb)
 
-cb = ttk.Combobox(root, values=("A", "B", "C", "D", "E"))     
+cb = ttk.Combobox(root, values=("A", "B", "C", "D", "E"))
 cb.set("A")
 cb.pack()
 cb.bind('<<ComboboxSelected>>', on_select)
 
 all_comboboxes.append(cb)
 
-b = tk.Button(root, text="Show selection", command=on_select)
+b = tk.Button(root, text="Show all selections", command=on_select)
 b.pack()
 
 root.mainloop()
-
