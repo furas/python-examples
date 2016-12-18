a8a79e26a380b8d396bf412eba56535342502027
Bartlomiej "furas" Burek <furas@tlen.pl>
2016-12-18 22:47:32 +0100

    follow player

---
+++ pygame/follow-player/README.md
@@ -0,0 +1,1 @@
+Use arrows to move player (green) and mosters will follow (red and blue)
---
+++ pygame/follow-player/main.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+#
+# pygame (functions) template - by furas
+#
+
+# ---------------------------------------------------------------------
+
+__author__  = 'Bartlomiej "furas" Burek'
+__email__   = 'furas@tlen.pl'
+__webpage__ = 'http://blog.furas.pl'
+
+# ---------------------------------------------------------------------
+
+import pygame
+
+# === CONSTANS === (UPPER_CASE names)
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 255)
+
+SCREEN_WIDTH  = 600
+SCREEN_HEIGHT = 400
+
+BLOCK_SIZE = 50
+
+# === CLASSES === (CamelCase names)
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface((50,50))
+        self.image.fill(GREEN)
+
+        self.rect = self.image.get_rect()
+        self.rect.center = screen_rect.center
+        
+        self.move_x = 0
+        self.move_y = 0
+        self.gravity = 1
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def update(self):
+        self.rect.x += self.move_x
+        self.rect.y += self.move_y
+        #self.jump -= self.gravity
+
+    def handle_event(self, event):
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.move_x -= 10
+            elif event.key == pygame.K_RIGHT:
+                self.move_x += 10
+            elif event.key == pygame.K_UP:
+                self.move_y -= 10
+            elif event.key == pygame.K_DOWN:
+                self.move_y += 10
+            
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+               self.move_x += 10
+            elif event.key == pygame.K_RIGHT:
+                self.move_x -= 10
+            elif event.key == pygame.K_UP:
+                self.move_y += 10
+            elif event.key == pygame.K_DOWN:
+                self.move_y -= 10
+    
+class Moster(pygame.sprite.Sprite):
+
+    def __init__(self, speed=1, color=RED):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface((50,50))
+        self.image.fill(color)
+
+        self.rect = self.image.get_rect()
+        self.rect.center = screen_rect.center
+        
+        self.move_x = 0
+        self.move_y = 0
+        self.gravity = 1
+
+        self.speed = speed
+        
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def update(self, player):
+        dif_x = player.rect.x - self.rect.x
+        if dif_x > 0:
+            self.rect.x += self.speed
+        elif dif_x < 0:
+            self.rect.x -= self.speed
+            
+        dif_y = player.rect.y - self.rect.y
+        if dif_y > 0:
+            self.rect.y += self.speed
+        elif dif_y < 0:
+            self.rect.y -= self.speed
+
+# === MAIN === (lower_case names)
+
+# --- (global) variables --- 
+
+# --- init ---
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen_rect = screen.get_rect()
+
+# --- objects ---
+
+player = Player()
+monster1 = Moster(5, RED)
+monster2 = Moster(2, BLUE)
+
+# --- mainloop ---
+
+clock = pygame.time.Clock()
+is_running = True
+
+while is_running:
+
+    # --- events ---
+    
+    for event in pygame.event.get():
+
+        # --- global events ---
+        
+        if event.type == pygame.QUIT:
+            is_running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                is_running = False
+
+        # --- objects events ---
+
+        player.handle_event(event)
+        
+    # --- updates ---
+
+    player.update()
+    monster1.update(player)
+    monster2.update(player)
+    
+    # --- draws ---
+    
+    screen.fill(BLACK)
+
+    player.draw(screen)
+    monster1.draw(screen)
+    monster2.draw(screen)
+    
+    pygame.display.update()
+
+    # --- FPS ---
+
+    clock.tick(25)
+
+# --- the end ---
+    
+pygame.quit()
