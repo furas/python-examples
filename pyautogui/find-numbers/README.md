8c365def37908b2e0b7623a12d2239b87b8c0ee4
Bartłomiej "furas" Burek <furas@tlen.pl>
2017-03-06 00:57:57 +0100

    pyautogui get numbers

---
+++ pyautogui/find-numbers/README.md
@@ -0,0 +1,9 @@
+It finds digits on screenshot (or screen).
+
+[PyAutoGUI - screenshot](https://pyautogui.readthedocs.io/en/latest/screenshot.html)
+
+
+
+    apt-get install scrot # to grab screenshot
+    pip install pyautogui
+
---
+++ pyautogui/find-numbers/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import pyautogui
+
+def find(screenshot):
+
+    # dictionary for digits grouped by "y"
+    
+    found = dict()
+
+    # find all digits 
+    
+    for digit in range(10):
+        
+        positions = pyautogui.locateAll('digits/{}.png'.format(digit), screenshot, True)
+        
+        for x,y,_,_ in positions:
+            if y not in found:
+                found[y] = dict()
+            found[y][x] = digit
+
+    # create numbers 
+
+    result = []
+    
+    for row in sorted(found):
+        cols = sorted(found[row])
+        number = ''.join(str(found[row][col]) for col in cols)
+        result.append(number)
+        
+    return result
+
+if __name__ == '__main__':
+    #img = pyautogui.screenshot(region=(0, 0, 300, 400))
+    img = 'screenshot.png'
+
+    result = find(img)
+    
+    print('\n'.join(result))
