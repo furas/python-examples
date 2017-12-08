e625b267e892b2598335f13c9a5861f37cf6db47
Bartłomiej "furas" Burek <furas@tlen.pl>
2017-12-08 21:42:17 +0100

    scrapy-tripadvisor.com-get_reviews

---
+++ scrapy/tripadvisor.com/README.md
@@ -0,0 +1,20 @@
+## get-reviews.py
+
+I use normal url - without `-or{}` - to get page and find number of reviews.  
+Next I add `-or{}` to url - it can be in any place - to generate urls to pages with reviews.  
+And then I use `for` loop and `Request()` to get pages with reviews.  
+Reviews are parsed by different method - `parse_reviews()`.
+
+I use `scrapy.crawler.CrawlerProcess()` to run it without full project, so everyone can easily run and test it.
+
+It saves data in `output.csv`.
+
+---
+
+**BTW:** to get page you need urls like
+
+ https://www.tripadvisor.com/g60795-d102542
+ https://www.tripadvisor.com/g60795-d102542-or0
+ https://www.tripadvisor.com/g60795-d102542-or5
+
+Other words in url are only for `SEO` - to get better position in Google search results.
---
+++ scrapy/tripadvisor.com/get_reviews.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import scrapy
+
+class MySpider(scrapy.Spider):
+    
+    name = 'myspider'
+    allowed_domains = ['tripadvisor.com']
+
+    start_urls = [
+        'https://www.tripadvisor.com/Hotel_Review-g60795-d102542-Reviews-Courtyard_Philadelphia_Airport-Philadelphia_Pennsylvania.html',
+        'https://www.tripadvisor.com/Hotel_Review-g60795-d122332-Reviews-The_Ritz_Carlton_Philadelphia-Philadelphia_Pennsylvania.html',
+    ]
+
+    def parse(self, response):
+        # get number of reviews
+        num_reviews = response.css('span.reviews_header_count::text').extract_first()
+        num_reviews = num_reviews[1:-1] # remove `( )`
+        num_reviews = num_reviews.replace(',', '') # remove `,`
+        num_reviews = int(num_reviews)
+        print('num_reviews:', num_reviews, type(num_reviews))
+        
+        # create template for urls to pages with reviews
+        url = response.url.replace('.html', '-or{}.html')
+        print('template:', url)
+        
+        # add requests to list
+        for offset in range(0, num_reviews, 5):
+            print('url:', url.format(offset))
+            yield scrapy.Request(url=url.format(offset), callback=self.parse_reviews)
+
+    def parse_reviews(self, response):
+        print('review:', response.url)
+        for idx,review in enumerate(response.css('div.review-container')):
+            item = {
+                'num_reviews': response.css('span.reviews_header_count::text')[0].re(r'\d{0,3}\,?\d{1,3}'),
+                'hotel_name': response.css('h1.heading_title::text').extract_first(),
+                'review_title': review.css('span.noQuotes::text').extract_first(),
+                'review_body': review.css('p.partial_entry::text').extract_first(),
+                'review_date': review.xpath('//*[@class="ratingDate relativeDate"]/@title')[idx].extract(),
+                'num_reviews_reviewer': review.css('span.badgetext::text').extract_first(),
+                'reviewer_name': review.css('span.scrname::text').extract(),
+                'bubble_rating': review.xpath("//div[contains(@class, 'reviewItemInline')]//span[contains(@class, 'ui_bubble_rating')]/@class")[idx].re(r'(?<=ui_bubble_rating bubble_).+?(?=0)')
+            }
+            yield item
+        
+        
+# --- run without project ---
+
+import scrapy.crawler
+
+c = scrapy.crawler.CrawlerProcess({
+    "FEED_FORMAT": 'csv',
+    "FEED_URI": 'output.csv',
+})
+c.crawl(MySpider)
+c.start()
