93bfed09c6a8d204d09df160aaff4c49693c1bc3
Bartłomiej "furas" Burek <furas@tlen.pl>
2018-05-05 01:33:36 +0200

    updates

--- __scraping__/tripadvisor.com - requests/get_reviews.py
+++ __scraping__/tripadvisor.com - requests/get_reviews.py
@@ -6,64 +6,81 @@
 
 import requests               # to get html from server
 from bs4 import BeautifulSoup # to search in html
-import re
-#import webbrowser             # to open html file in web browser
+#~ import webbrowser             # to open (downloaded) html file in web browser
 
 # global variable to keep request session with all cookies, etc.
 s = requests.Session()
 
-# NEW: get reviews in all languages - doesn't work, it gets only english reviews but code gets number of all reviews :(
+# NEW: get reviews in all languages - doesn't work, it gets only english reviews but page gives number of all reviews :(
 s.cookies.set('TALanguage', 'ALL', domain='.google.co.uk', path='/') # other lanugages ie. 'en', 'es'
 
+
 def get_soup(url):
+    '''Read HTML from server and convert to Soup'''
     
+    # some portals send correct HTML only if you have correct header 'user-agent'
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'}
     
+    # get response with HTML
     r = s.get(url, headers=headers)
     
-    # write html in file and open it in web browser - to see what you get from server
-    #with open('temp.html', 'wb') as f:
-    #    f.write(r.content)
-    #    webbrowser.open('temp.html')
+    # write html in file temp.html and open it in web browser.
+    # it is useful to see/test what we get from server.
+    #~ with open('temp.html', 'wb') as f:
+    #~     f.write(r.content)
+    #~     webbrowser.open('temp.html')
         
-    if r.status_code != 200:
-        print('status code:', r.status_code)
+    # check if 
+    if r.status_code != 200: # not OK
+        print('[get_soup] status code:', r.status_code)
+        # (as default) it will returns None instead of Soup
     else:
         return BeautifulSoup(r.text, 'html.parser')
 
     
 def parse(url, response):
+    '''Get number of reviews and start getting subpages with reviews'''
     
+    print('[parse] url:', url)
+
     if not response:
-        print('no response:', url)
+        print('[parse] no response:', url)
         return
     
     # get number of reviews
-    num_reviews = response.find('span', class_='reviews_header_count').text
+    num_reviews = response.find('span', class_='reviews_header_count').text # get text
     num_reviews = num_reviews[1:-1] # remove `( )`
     num_reviews = num_reviews.replace(',', '') # remove `,`
-    num_reviews = int(num_reviews)
-    print('num_reviews:', num_reviews, type(num_reviews))
+    num_reviews = int(num_reviews) # convert text into integer
+    print('[parse] num_reviews:', num_reviews, type(num_reviews))
     
-    # create template for urls to pages with reviews
-    url = url.replace('.html', '-or{}.html')
-    print('template:', url)
+    # create template url to subpages with reviews
+    # ie. https://www.tripadvisor.com/Hotel_Review-g562819-d289642-or{}.html
+    url_template = url.replace('.html', '-or{}.html')
+    print('[parse] url_template:', url_template)
     
-    # add requests to list
+    # get subpages and parse reviwes.
+    # every subpage has 5 reviews so pages has urls with -or0 -or5 -or10 etc.
     for offset in range(0, num_reviews, 5):
-        print('url:', url.format(offset))
-        url_ = url.format(offset)
-        parse_reviews(url_, get_soup(url_))
-        #return # for test only - to stop after first page
+        subpage_url = url_template.format(offset)
+        
+        print('[parse] subpage_url:', subpage_url)
+        
+        parse_reviews(subpage_url, get_soup(subpage_url))
+        
+        #~ return # for test only - to stop after first page
 
 
 def parse_reviews(url, response):
-    print('review:', url)
+    '''Get all reviews from one page'''
+    
+    print('[parse_reviews] url:', url)
 
     if not response:
-        print('no response:', url)
+        print('[parse_reviews] no response:', url)
         return
     
+    # find all reviews on page 
     for idx, review in enumerate(response.find_all('div', class_='review-container')):
         
         # NEW: works - it has to check if `badgetext` exists on page
@@ -78,7 +95,7 @@ def parse_reviews(url, response):
             'review_title': review.find('span', class_='noQuotes').text,
             'review_body': review.find('p', class_='partial_entry').text,
             #'review_date': review.find('span', class_='relativeDate')['title'],#.text,#[idx], # OLD: doesn't work
-            'review_date': review.find('span', class_='ratingDate')['title'],#.text,#[idx],
+            'review_date': review.find('span', class_='ratingDate')['title'],#.text,#[idx], # NEW: works - 'ratingDate' instead of 'relativeDate'
             
             #'num_reviews_reviewer': review.find('span', class_='badgetext').text, # OLD: doesn't work 
             'num_reviews_reviewer': badgetext, # NEW: works - it has to check if `badgetext` exists on page
@@ -86,18 +103,26 @@ def parse_reviews(url, response):
             #'reviewer_name': review.find('span', class_='scrname').text, # OLD: doesn't work
             #'bubble_rating': review.select_one('div.reviewItemInline span.ui_bubble_rating')['class'][1][7:], # OLD: doesn't work
         }
-        #~ yield item
+        #~ yield item # generator which returns items 
+        
+        # display on screen 
+        print('--- review ---')
         for key,val in item.items():
-            print(key, ':', val)
-        print('----')
-        #return # for test only - to stop after first review
+            print(' ', key, ':', val)
+        
+        #~ return # for test only - to stop after first review
 
-# some URLs for testing code        
-start_urls = [
-    'https://www.tripadvisor.com/Hotel_Review-g562819-d289642-Reviews-Hotel_Caserio-Playa_del_Ingles_Maspalomas_Gran_Canaria_Canary_Islands.html',
-    #'https://www.tripadvisor.com/Hotel_Review-g60795-d102542-Reviews-Courtyard_Philadelphia_Airport-Philadelphia_Pennsylvania.html',
-    #'https://www.tripadvisor.com/Hotel_Review-g60795-d122332-Reviews-The_Ritz_Carlton_Philadelphia-Philadelphia_Pennsylvania.html',
-]
 
-for url in start_urls:
-    parse(url, get_soup(url))
+#----------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # some URLs for testing
+    start_urls = [
+        'https://www.tripadvisor.com/Hotel_Review-g562819-d289642-Reviews-Hotel_Caserio-Playa_del_Ingles_Maspalomas_Gran_Canaria_Canary_Islands.html',
+        #'https://www.tripadvisor.com/Hotel_Review-g60795-d102542-Reviews-Courtyard_Philadelphia_Airport-Philadelphia_Pennsylvania.html',
+        #'https://www.tripadvisor.com/Hotel_Review-g60795-d122332-Reviews-The_Ritz_Carlton_Philadelphia-Philadelphia_Pennsylvania.html',
+    ]
+
+    for url in start_urls:
+        parse(url, get_soup(url))
