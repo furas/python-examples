f2cc913132244d623ee3134d22b15fae61f1f23a
Bartłomiej "furas" Burek <furas@tlen.pl>
2018-06-01 01:25:28 +0200

    kivy grid in grid

--- kivy/grid-inside-grid/example2-widget-with-grid/mainwidget.py
+++ kivy/grid-inside-grid/example2-widget-with-grid/mainwidget.py
@@ -4,6 +4,8 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 
+#from kivy.core.window import Window
+#Window.clearcolor = (1, 1, 1, 1)
 
 class MainWidget(Widget):
     pass
