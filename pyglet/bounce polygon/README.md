3b5265508ee020b35d48d20bb9421e5328f95250
Bartłomiej "furas" Burek <furas@tlen.pl>
2018-06-08 02:32:23 +0200

    bouncing polygon

---
+++ pyglet/bounce polygon/README.md
@@ -0,0 +1,4 @@
+
+# Bouncing Polygon
+
+![#1](images/output.gif?raw=true)   
--- pyglet/bounce polygon/main.py
+++ pyglet/bounce polygon/main.py
@@ -2,9 +2,7 @@ import pyglet
 
 polygon_points = (10, 15, 30, 35, 50, 15)
 
-
 window = pyglet.window.Window()
-
 
 @window.event
 def on_draw():
@@ -17,15 +15,13 @@ def on_draw():
         ('v2i', polygon_points)
     )
 
-
 def update(dt):
     global polygon_points # use external variable instead creating local
     
-    # move every point in polygon (in X and Y direction) 
-    polygon_points = tuple(x+2 for x in polygon_points)
+    # move every point in polygon a little
+    polygon_points = tuple(x+1 for x in polygon_points)
     
     # print(dt) time between frames - you can use it to make smother move
-
 
 # execute `update` 60 times per second - it gives 60 FPS (Frames Per Second)    
 pyglet.clock.schedule_interval(update, 1/60.0)    
