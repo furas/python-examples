8b4d5f44034bf7c1c96ede373eb1f03458c896fb
Bartłomiej "furas" Burek <furas@tlen.pl>
2018-07-10 21:32:46 +0200

    pyqt5 drag frameless window

--- pyqt5/drag-frameless-window/main.py
+++ pyqt5/drag-frameless-window/main.py
@@ -7,37 +7,48 @@ class MyWindow(QtWidgets.QWidget):
     relative_y = None
     drag = None
     
-    def mouseReleaseEvent(self, event):
-        if event.button() == 1: # left
-            self.drag = False
-        
     def mousePressEvent(self, event):
+        # left - start dragging
         if event.button() == 1: # left
             self.drag = True
-            # mouse position
+            
+            # mouse start position on screeen
             point = event.screenPos()
-            # window position
+            
+            # window start position on screen
             rect = self.geometry()
         
-            # distance between both
+            # distance between mouse and left, top corner of window
             self.relative_x = point.x() - rect.x()
             self.relative_y = point.y() - rect.y()
 
+        # right - close application
         if event.button() == 2: # right
             self.close()
 
+        # middle - something different
         if event.button() == 4: # middle
             print('middle')
 
     def mouseMoveEvent(self, event):
+        # drag window
         if self.drag:
-            # mouse position
+            # mouse current position on screen
             point = event.screenPos()
-            # window new position
+            
+            # calculate window new position on screen
             new_x = point.x() - self.relative_x
             new_y = point.y() - self.relative_y
-            self.move(new_x, new_y)
             
+            # move window
+            self.move(new_x, new_y)
+
+    def mouseReleaseEvent(self, event):
+        # stop dragging
+        if event.button() == 1: # left
+            self.drag = False
+
+
 # Linux Mint 18.3 problem with style (GTK2/GTK3) 
 # "QApplication: invalid style override passed, ignoring it."
 #sys.argv += ['--style', 'Fusion'] 
