8e4ce7f987821e1f0f8e9f3523ac6743640d3873
Bartłomiej "furas" Burek <furas@tlen.pl>
2018-07-25 01:51:00 +0200

    CV2 stream update

--- cv2/display-stream/example-main.py
+++ cv2/display-stream/example-main.py
@@ -14,12 +14,12 @@ import cv2
 #stream = 'http://media.dumpert.nl/tablet/9f7c6290_Verstappen_vs._Rosberg_with_Horner_Smile___Streamable.mp4.mp4.mp4'
 
 # --- rtsp stream ---
-# doesn't work any more
-#stream = 'rtsp://granton.ucs.ed.ac.uk/domsdemo/v2003-1.wmv'
-
 stream = 'rtsp://streaming1.osu.edu/media2/ufsap/ufsap.mov'
 
-# --- local (build-in) camera ---
+# --- rtmp stream ---
+#stream = 'rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4'
+
+# --- local (built-in) camera ---
 #stream = 0
 
 
@@ -28,6 +28,7 @@ stream = 'rtsp://streaming1.osu.edu/media2/ufsap/ufsap.mov'
 # open stream
 cap = cv2.VideoCapture(stream)
 
+# check if opened 
 if not cap.isOpened():
     print("Can't open stream/file")
 else:    
@@ -35,7 +36,7 @@ else:
         # read one frame (and "return" status)
         ret, frame = cap.read()
 
-        # exit if error
+        # exit if error (check it because it doesn't rise error when it has problem to read)
         if not ret:
             break
 
@@ -44,7 +45,7 @@ else:
 
         # exit if pressed any key
         # (it doesn't wait for key so you can read next frame)
-        # (you have to one window to catch pressed key)
+        # (you need opened window to catch pressed key)
         if cv2.waitKey(1) != -1:
             break
 
