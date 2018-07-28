f506f99f26d6c12483e1e11b0c2fecc7455097fb
Bartłomiej "furas" Burek <furas@tlen.pl>
2018-07-28 05:07:07 +0200

    Google Geocoding API - lat, long example

--- Google API/Geocoding/main.py
+++ Google API/Geocoding/main.py
@@ -5,6 +5,8 @@ API_KEY = '<YOUR-API-KEY>'
 
 gmaps = googlemaps.Client(key=API_KEY)
 
+# convert address to lat,long
+
 results = gmaps.geocode('221B Baker Street, London, United Kingdom')
 
 for key in results[0].keys():
@@ -27,7 +29,13 @@ print('formatted_address:', item['formatted_address'])
 print('geometry:', item['geometry'])
 print('place_id:', item['place_id'])
 print('types:', item['types'])
-print('-----')
-    
+print('-----------------------------------------')
 
-    
+# convert lat,long to (nearest?) addresses
+
+result = gmaps.geocode('51.523767 -0.1585557')
+
+for item in result:
+    for part in item['address_components']:
+        print(part['long_name'])
+    print('---')
