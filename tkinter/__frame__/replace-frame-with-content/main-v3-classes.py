1e7551bb064c66805f5c05264fbaf3a3769ebdf6
Bartłomiej "furas" Burek <furas@tlen.pl>
2019-05-04 20:50:09 +0200

    new examples

--- tkinter/__frame__/replace-frame-with-content/main-v3-classes.py
+++ tkinter/__frame__/replace-frame-with-content/main-v3-classes.py
@@ -2,17 +2,40 @@ import tkinter as tk
 
 # --- classes ---
 
-def Page(tk.Frame):
+class MainFrame(tk.Frame):
 
-    def __init __(self, master):
-        super().__init__(self, master)
+    def __init__(self, master):
+        super().__init__(master)
 
-        button = tk.Button(main_frame, text="Frame #1", command=lambda:change_frame(frame_1))
+        button = tk.Button(self, text="Frame #1", command=lambda:change_frame(frame_1))
         button.pack()
 
-        button = tk.Button(main_frame, text="Frame #2", command=lambda:change_frame(frame_2))
+        button = tk.Button(self, text="Frame #2", command=lambda:change_frame(frame_2))
         button.pack()
 
+
+class Frame1(tk.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        l = tk.Label(self, text="It is Frame #1", bg='red')
+        l.pack()
+
+        b = tk.Button(self, text="BACK", command=lambda:change_frame(main_frame))
+        b.pack()
+
+
+class Frame2(tk.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        l = tk.Label(self, text="It is Frame #2", bg='green')
+        l.pack()
+
+        b = tk.Button(self, text="BACK", command=lambda:change_frame(main_frame))
+        b.pack()
 
 # --- functions ---
 
@@ -26,44 +49,18 @@ def change_frame(new_frame):
     current = new_frame
     current.pack()
 
-# ---
-
-def show_main_frame():
-    change_frame(main_frame)
-
-def show_frame_1():
-    change_frame(frame_1)
-
-def show_frame_2():
-    change_frame(frame_2)
-
 # --- main ---
 
 root = tk.Tk()
 
+# --- pages without .pack() before Main Page---
+
+frame_1 = Frame1(root)
+frame_2 = Frame2(root)
+
 # --- main frame without .pack() ---
 
-main_frame = tk.Frame(root)
-
-# --- tk.Frame ---
-
-frame_1 = tk.Frame(root)
-
-l = tk.Label(frame_1, text="It is Frame #1", bg='red')
-l.pack()
-
-b = tk.Button(frame_1, text="BACK", command=lambda:change_frame(main_frame))
-b.pack()
-
-# --- tk.Frame without .pack() ---
-
-frame_2 = tk.Frame(root)
-
-l = tk.Label(frame_2, text="It is Frame #2", bg='green')
-l.pack()
-
-b = tk.Button(frame_2, text="BACK", command=lambda:change_frame(main_frame))
-b.pack()
+main_frame = MainFrame(root)
 
 # ---
 
