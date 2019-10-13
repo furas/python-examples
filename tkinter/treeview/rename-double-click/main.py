7078d807c61048ed91a0e174048597303e7f13cb
Bartłomiej "furas" Burek <furas@tlen.pl>
2019-10-13 16:25:23 +0200

    new examples 2019.10.13

--- tkinter/treeview/rename-double-click/main.py
+++ tkinter/treeview/rename-double-click/main.py
@@ -6,41 +6,54 @@ import tkinter as tk
 from tkinter import ttk
 
 # --- functions ---
- 
-def rename(event=None): # `event` to get argument from double click
-    top = tk.Toplevel()
-    name_entry = tk.Entry(top)
-    name_entry.pack()
 
-    def on_enter(event):
-        selected = tree.selection()[0]
-        print(selected)
-        print(tree.item(selected)['text'], name_entry.get())
-        #tree.item(selected)['text'] = name_entry.get()
-        tree.item(selected, text=name_entry.get())
-        print(tree.item(selected)['text'], name_entry.get())
-        top.destroy()
-
-    name_entry.bind('<Return>', on_enter)
- 
 def add():
     top = tk.Toplevel()
-    tk.Label(top, text="Name").pack()
+    tk.Label(top, text="Add").pack()
     name_entry = tk.Entry(top)
     name_entry.pack()
 
-    def on_OK():
-        item = tree.insert(tree.selection(), "end", text=name_entry.get())
+    def on_OK(event=None): # `event` to get argument from `bind <Return>`
+        if tree.selection():
+            place = tree.selection()[0]
+        else:
+            place = ''
+        item = tree.insert(place, "end", text=name_entry.get())
         tree.item(item, tag=item) # add tag to bind mouse click
         tree.tag_bind(item, '<Double-Button-1>', rename) # rename on double click
         top.destroy()
 
+    name_entry.bind('<Return>', on_OK)
     tk.Button(top, text="OK", command=on_OK).pack(fill='x')
-    tk.Button(top, text="Cancel", command=quit).pack(fill='x')
+    tk.Button(top, text="Cancel", command=top.destroy).pack(fill='x')
+ 
+def rename(event=None): # `event` to get argument from `bind <Double-Button-1>`
+    top = tk.Toplevel()
+    tk.Label(top, text="Rename").pack()
+    name_entry = tk.Entry(top)
+    name_entry.pack()
+
+    def on_OK(event=None): # `event` to get argument from `bind <Return>`
+        for item in tree.selection():
+            tree.item(item, text=name_entry.get())
+        top.destroy()
+
+    name_entry.bind('<Return>', on_OK)
+    tk.Button(top, text="OK", command=on_OK).pack(fill='x')
+    tk.Button(top, text="Cancel", command=top.destroy).pack(fill='x')
  
 def delete():
-    if tree.selection():
-        tree.delete(tree.selection())
+    top = tk.Toplevel()
+    tk.Label(top, text="Delete").pack()
+    tk.Label(top, text="Are you sure?").pack()
+    
+    def on_OK(event=None): # `event` to get argument from `bind <Return>`
+        for item in tree.selection():
+            tree.delete(item)
+        top.destroy()
+ 
+    tk.Button(top, text="OK", command=on_OK).pack(fill='x')
+    tk.Button(top, text="Cancel", command=top.destroy).pack(fill='x')
  
 # --- main ---
 
