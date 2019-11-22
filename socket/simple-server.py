d53ec5636430834924ba3bcb95aeb93348233eb9
Bartłomiej "furas" Burek <furas@tlen.pl>
2019-11-22 18:15:22 +0100

    new examples

---
+++ socket/simple-server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3 
+
+import socket
+
+HOST, PORT = '', 8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# to solve problem with 
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+
+print(f'Serving HTTP on port {PORT} ...')
+
+while True:
+
+    # ---
+    
+    print('start ')
+    
+    # --- accept new client ---
+    
+    client_connection, client_address = listen_socket.accept()
+
+    # --- receive request ---
+        
+    request_data = b''
+    
+    while True:
+        request_data += client_connection.recv(1) # get by char to easier recognize end of header
+        if request_data.endswith(b'\r\n\r\n'): # recognize end of header - empty line
+            break
+    
+    print(request_data.decode('utf-8'))
+    
+    # --- send response ---
+    
+    client_connection.sendall(b"""HTTP/1.1 200 OK\n\nHello, World 3@!\n""")
+    
+    # --- close connection to client ---
+    
+    client_connection.close()
+    
+    # ----
+    
+    print('end')
