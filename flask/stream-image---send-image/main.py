994b12f8f3837f77d4c6fe95b5b312aa280fb24d
furas <furas@tlen.pl>
2022-06-08 02:51:18 +0200

    flask

--- flask/stream-image---send-image/main.py
+++ flask/stream-image---send-image/main.py
@@ -18,7 +18,8 @@ def get_image():
         yield(b'--frame\r\n'
               b'Content-Type: image/jpeg\r\n'
               b'\r\n'+ img + b'\r\n')
-        time.sleep(0.01) # my Firefox needs some time to display image / Chrome displays image without it
+        time.sleep(0.04) # my Firefox needs some time to display image / Chrome displays image without it
+                         # 0.04s = 40ms = 25 frames per second (this can be enough)
 
 
 def get_image_with_size():
@@ -28,11 +29,12 @@ def get_image_with_size():
               b'Content-Type: image/jpeg\r\n'
               b'Content-Length: ' + length + b'\r\n'
               b'\r\n'+ img + b'\r\n')
-        time.sleep(0.01) # my Firefox needs some time to display image / Chrome displays image without it
-
+        time.sleep(0.04) # my Firefox needs some time to display image / Chrome displays image without it
+                         # 0.04s = 40ms = 25 frames per second (this can be enough) 
 
 @app.route("/stream")
 def stream():
+    # if you use `boundary=other_name` then you have to yield `b--other_name\r\n`
     return Response(get_image_with_size(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
