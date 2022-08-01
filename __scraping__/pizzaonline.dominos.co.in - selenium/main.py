95cb53b664f312e0830f010c0c96be94d4a4db90
furas <furas@tlen.pl>
2022-08-01 16:48:33 +0200

    __scraping__

--- __scraping__/pizzaonline.dominos.co.in - selenium/main.py
+++ __scraping__/pizzaonline.dominos.co.in - selenium/main.py
@@ -11,27 +11,33 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import time
 
-# start web browser
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 #driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
-# get source code
+# open page
 driver.get('https://pizzaonline.dominos.co.in/menu')
 
-# it would need to scroll page by page to get images
+# it would need to scroll page by page to get images from `src` but it has also image in `data-src` which doesn't need to scroll
 #driver.execute_script("var scrollingElement = (document.scrollingElement || document.body); scrollingElement.scrollTop = scrollingElement.scrollHeight;")
 
+# wait for changes
 time.sleep(3)
 
+# get HTML (after adding elements by JavaScript)
 html = driver.page_source
 
+# search all items
 all_items = driver.find_elements(By.XPATH, '//div[@class="itm-wrppr"]/div')
 print('len(all_items):', len(all_items))
 
 for number, item in enumerate(all_items, 1):
+
+    # it keeps name also in <div data-label="...">
     name = item.get_attribute('data-label')
+
     # get `data-src` instead of `src` because it uses `lazy-loading`
-    image = item.find_element(By.XPATH, './/div[@class="img-cvr"]//img').get_attribute('data-src')  
+    image = item.find_element(By.XPATH, './/div[@class="img-cvr"]//img').get_attribute('data-src')
+
     print(f' {number:3} | {name:60} | {image}')
 
 # close web browser
